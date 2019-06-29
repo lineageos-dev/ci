23df9363f96625bdeee297cf88b567bcb3d51d39
pexcn <i@pexcn.me>
2019-06-29 14:12:53 +0800

    restore to run with machine
    ram is too small in docker mode

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -1,6 +1,41 @@
 #!/bin/bash -e
 
-sudo dd if=/dev/zero of=/swap count=4096 bs=1M || true
-sudo chmod 600 /swap || true
-sudo mkswap /swap || true
-sudo swapon /swap || true
+# non-interactive
+export DEBIAN_FRONTEND=noninteractive
+echo 'APT::Get::Assume-Yes "true";' | sudo tee -a /etc/apt/apt.conf.d/90non-interactive
+echo 'DPkg::Options "--force-confnew";' | sudo tee -a /etc/apt/apt.conf.d/90non-interactive
+
+# dependencies
+sudo apt-get update
+sudo apt-get install --no-install-recommends -y \
+  apt-utils dialog locales sudo ca-certificates ccache parallel \
+  net-tools netcat nano bzip2 gzip tar xz-utils curl wget \
+  bc bison build-essential flex g++-multilib gcc-multilib \
+  git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev \
+  lib32z-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev \
+  libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool \
+  squashfs-tools xsltproc openjdk-8-jdk python zip unzip zlib1g-dev \
+  libc6-dev-i386 x11proto-core-dev libx11-dev libgl1-mesa-dev
+sudo apt-get clean
+
+# repo
+sudo curl -sSL https://storage.googleapis.com/git-repo-downloads/repo -o /usr/local/bin/repo
+sudo chmod a+x /usr/local/bin/repo
+
+# timezone
+sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# swap
+sudo dd if=/dev/zero of=/swap count=4096 bs=1M
+sudo chmod 600 /swap
+sudo mkswap /swap
+sudo swapon /swap
+
+# git
+git config --global user.name "Builder"
+git config --global user.email "build@pexcn.me"
+git config --global color.ui auto
+git config --global log.date iso
+
+# ccache
+ccache -M 20G
