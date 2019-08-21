fad419696e3262a186d809df493eeeb87dfecf78
pexcn <i@pexcn.me>
2019-08-22 00:44:05 +0800

    update local manifests url

--- build.sh
+++ build.sh
@@ -9,10 +9,8 @@ get_sources() {
   cd lineageos
 
   repo init -u https://github.com/LineageOS/android.git -b cm-14.1 --depth 1
-  curl -sSL --create-dirs -o .repo/local_manifests/vendor.xml \
-    https://raw.githubusercontent.com/hammerhead-dev/android_local_manifests_for_ci/cm-14.1/local_manifest.xml
-  curl -sSL --create-dirs -o .repo/local_manifests/remove.xml \
-    https://raw.githubusercontent.com/hammerhead-dev/android_local_manifests_for_ci/cm-14.1/remove.xml
+  curl -sSL --create-dirs -o .repo/local_manifests/vendor.xml https://github.com/lineageos-dev/android_local_manifests/raw/cm-14.1/vendor.xml
+  curl -sSL --create-dirs -o .repo/local_manifests/remove.xml https://github.com/lineageos-dev/android_local_manifests/raw/cm-14.1/remove.xml
   repo sync -c --no-tags --no-clone-bundle -j8
 
   cd $CUR_DIR
